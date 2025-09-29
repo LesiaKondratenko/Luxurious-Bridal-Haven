b7838d6f50ea90b9e1425b08d3e2dbae3b7e90c5
Lesia <lesyastepanenko1994@gmail.com>
2025-09-29 20:48:37 +0300

    added reviews

--- app/js/main.js
+++ app/js/main.js
@@ -1,9 +1,24 @@
 const swiper = new Swiper('.accessories__slider', {
   slidesPerView: 3,
   spaceBetween: 40,
+  loop: true,
   navigation: {
     nextEl: '.accessories__arrow--next',
     prevEl: '.accessories__arrow--prev',
   },
+});
+
+const swiperReviews = new Swiper('.reviews-slider', {
+  slidesPerView: 11,
+  spaceBetween: 16,
   loop: true,
+  navigation: {
+    nextEl: '.reviews-slider-next',
+    prevEl: '.reviews-slider-prev',
+  },
+     pagination: {
+        el: ".reviews-slider-pagination",
+        type: "fraction",
+      },
+
 });
